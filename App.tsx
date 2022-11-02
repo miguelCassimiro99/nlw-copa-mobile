a674796e9cff4dc5330cb2e0ad3d4c2caa6cf9ff
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2022-11-02 10:17:11 -0300

    mobile app base setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NativeBaseProvider, VStack, Text } from "native-base";
+import { THEME } from './src/styles/theme'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/Signin';
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <VStack flex={1} bgColor="gray.900">
+        {
+          fontsLoaded ? <SignIn /> : <Loading />
+        }
+      </VStack>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//? Native Base as Components Library
